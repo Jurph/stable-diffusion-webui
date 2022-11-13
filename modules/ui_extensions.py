3ca792013b6cd3fad8e1b6c04c1af295538e6196
Jurph <Jurph@users.noreply.github.com>
2022-11-13 11:14:05 -0500

    Clarify error message
    Added clearer instructions for resolving this error message.

--- modules/ui_extensions.py
+++ modules/ui_extensions.py
@@ -17,7 +17,7 @@ available_extensions = {"extensions": []}
 
 
 def check_access():
-    assert not shared.cmd_opts.disable_extension_access, "extension access disabed because of commandline flags"
+    assert not shared.cmd_opts.disable_extension_access, "Extension updates are disabled when the UI is remotely accessible. Restart without the --listen or --share command line options and try again."
 
 
 def apply_and_restart(disable_list, update_list):
